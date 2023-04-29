0c9d995a94d2c774b901ec675e0e2a5f1fd9a40e
Eduardo <eduardo.alonso.73h@gmail.com>
2023-04-29 15:59:42 -0600

    Update twitter4j version 4.0.7 to 4.1.2

--- app/src/main/java/com/jorgesys/androidtwitter4j/MainActivity.java
+++ app/src/main/java/com/jorgesys/androidtwitter4j/MainActivity.java
@@ -6,24 +6,21 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import twitter4j.OAuthAuthorization;
+import twitter4j.RequestToken;
 
-import twitter4j.Twitter;
-import twitter4j.TwitterFactory;
-import twitter4j.auth.RequestToken;
-import twitter4j.conf.ConfigurationBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Twitter4j";
-    private static Twitter twitter;
-    private static final String APIConsumerKey = "qW5IFxldNY0wSPEw8vTw9Q";
-    private static final String APIConsumerSecretKey = "ZZaEgWN1fi77ilrDGHPWqd4ZsdN63R4nDqD1Pqzi6M";
+    private static final String APIConsumerKey = "dW6bW6H6kEzLa95dWLq3i0w4I";
+    private static final String APIConsumerSecretKey = "U0ZQUWhZJK1yYBRVmrcIoXrm8O4fyvsEooNDFhOOuBI3zXJHyp";
     private static final String CALLBACK_URL = "http://www.stackoverflow.com"; //"x-oauthflow-twitter://twitterlogin";
-    private static RequestToken twitterRequestToken;
     private EditText twitterUser;
+    private static RequestToken requestToken;
+    private static OAuthAuthorization oAuthAuthorization ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,38 +32,24 @@ public class MainActivity extends AppCompatActivity {
 
         twitterUser = (EditText)findViewById(R.id.edtTwitterUser);
 
-        ((Button)findViewById(R.id.btnSendTweet)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sendTweet();
-            }
-        });
+        ((Button)findViewById(R.id.btnSendTweet)).setOnClickListener(view -> sendTweet());
     }
 
-    private static void initializeTwitter(final Activity activity) {
-        ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
-        configurationBuilder.setOAuthConsumerKey(APIConsumerKey);
-        configurationBuilder.setOAuthConsumerSecret(APIConsumerSecretKey);
-        twitter = new TwitterFactory(configurationBuilder.build()).getInstance();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    twitterRequestToken = twitter.getOAuthRequestToken(CALLBACK_URL);
-                    activity.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            //Open Dialog for authentication.
-                            Intent intent = new Intent(activity, TwitterAuthActivity.class);
-                            if (twitterRequestToken != null) {
-                                intent.putExtra("TwitterReqTokenUrl", twitterRequestToken.getAuthenticationURL());
-                                activity.startActivity(intent);
-                            }
-                        }
-                    });
-                } catch (Exception e) {
-                    Log.e(TAG, "Exception " + e.getMessage());
-                }
+    private static void initializeTwitter(final Activity activity){
+        new Thread(() -> {
+            try {
+                oAuthAuthorization=OAuthAuthorization.getInstance(APIConsumerKey,APIConsumerSecretKey);
+                requestToken=oAuthAuthorization.getOAuthRequestToken(CALLBACK_URL);
+                activity.runOnUiThread(() -> {
+                    //Open Dialog for authentication.
+                    Intent intent = new Intent(activity, TwitterAuthActivity.class);
+                    if (requestToken != null) {
+                        intent.putExtra("TwitterReqTokenUrl", requestToken.getAuthenticationURL());
+                        activity.startActivity(intent);
+                    }
+                });
+            } catch (Exception e) {
+                Log.e(TAG, "Exception " + e.getMessage());
             }
         }).start();
     }
